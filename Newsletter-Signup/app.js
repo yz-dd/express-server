1ae97515f5beaaa8d17de0af31fef54555e44dd4
tedballin_ <tedz@live.cn>
2020-04-19 15:12:28 -0700

    Posts Data to Mailchimp's server

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -25,9 +26,50 @@ app.post('/', (req, res) => {
   let lastName = req.body.lName;
   let email = req.body.Email;
 
-  console.log(firstName + ' ' + lastName + ' ' + email);
+  // create data as JSON to send to Mailchimp, object
+  //keys are from the API references
+  let data = {
+    members: {
+      email_address: email,
+      status: "subscribed",
+      //merge field is an object for first & last name
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }
+  };
+
+  //turn js object(data) into flat pack JSON
+  let jsonData = JSON.stringify(data);
+
+  //https://usX.api.mailchimp.com/3.0/lists/cb8320f1f8  replace X with server number in your API key
+  let url = 'https://us4.api.mailchimp.com/3.0/lists/cb8320f1f8';
+
+  let options = {
+    method: 'POST',
+    auth: 'Yang1:511da3c3f3927a88200d1fa0d018d689-us4'
+  };
+
+  //make request https request, then get response from mailchimp server
+ const request = https.request(url, options, (response) => {
+  response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
 });
+
+
 
 app.listen(3000, () => {
   console.log('server is running on port 3000');
 });
+
+//API key
+// 511da3c3f3927a88200d1fa0d018d689-us4
+//List ID
+// cb8320f1f8
